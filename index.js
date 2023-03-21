25681320e2e36a4159709acd955748d24042bd2a
yago <yagom10@hotmail.com>
2023-03-21 15:53:50 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+const num1 = 2
+const num2 = 31
+const multiply= num1 * num2 
+
+const random= Math.floor(Math.random() * 10) + 1;
+
+const num3 = 18
+const num4 = 7
+const mod = (num3) % (num4)
+
+const max = (5, 10, 15, 20)
